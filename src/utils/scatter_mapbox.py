161bb828858d1529d2acf6cf683141152278bcf3
Akihiro Tanaka <akihiro.fin@gmail.com>
2024-03-04 12:26:45 +0200

    Fix/plot data that has different lat/long codes

--- src/utils/scatter_mapbox.py
+++ src/utils/scatter_mapbox.py
@@ -5,15 +5,18 @@ import plotly.express as px
 
 def preprocess_data(df, genera, threshold):
     gdf = create_gdf(pd.DataFrame(df))
-    gdff = gdf[[genera, "LATSTR", "LONGSTR", "geometry", "COUNTRY", "NAME", "MIN_AGE", "MAX_AGE"]][gdf[genera] >= threshold]
+    try:
+        gdff = gdf[[genera, "LATSTR", "LONGSTR", "geometry", "COUNTRY", "NAME", "MIN_AGE", "MAX_AGE"]][gdf[genera] >= threshold]
+    except KeyError:
+        gdff = gdf[[genera, "LAT", "LONG", "geometry", "COUNTRY", "SITE_NAME", "MIN_AGE", "MAX_AGE"]][gdf[genera] >= threshold]
     return gdff
 
 def create_map_figure(gdff, genera):
     if len(gdff[genera].unique()) <= 8:
         gdff[genera] = gdff[genera].astype(str)
-        fig = px.scatter_mapbox(gdff, lat=gdff.geometry.y, lon=gdff.geometry.x, mapbox_style="open-street-map", zoom=3, color=genera, hover_data=["COUNTRY", "NAME", "MIN_AGE", "MAX_AGE"])
+        fig = px.scatter_mapbox(gdff, lat=gdff.geometry.y, lon=gdff.geometry.x, mapbox_style="open-street-map", zoom=1, color=genera, hover_data=["COUNTRY", "SITE_NAME", "MIN_AGE", "MAX_AGE"])
     else:
-        fig = px.scatter_mapbox(gdff, lat=gdff.geometry.y, lon=gdff.geometry.x, mapbox_style="open-street-map", zoom=3, color=genera, hover_data=["COUNTRY", "NAME", "MIN_AGE", "MAX_AGE"])
+        fig = px.scatter_mapbox(gdff, lat=gdff.geometry.y, lon=gdff.geometry.x, mapbox_style="open-street-map", zoom=1, color=genera, hover_data=["COUNTRY", "SITE_NAME", "MIN_AGE", "MAX_AGE"])
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_traces(marker={'size': 15, 'opacity': 0.6})
     return fig
