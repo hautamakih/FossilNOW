27ba57d86839454b2e0c2413e6c31b7a9eab8f9e
Akihiro Tanaka <akihiro.fin@gmail.com>
2024-03-31 02:00:12 +0200

    Initial knn

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ import plotly.express as px
 from utils.dataframe import *
 from utils.scatter_mapbox import preprocess_data, create_map_figure, add_convex_hull_to_figure, create_histo, add_top_n
 import numpy as np
+from models.models import get_recommend_list_mf, get_recommend_list_knn
 
 
 AGE_SPANS = ["0-0.1", "0.1-0.6", "0.6-1.1", "1.1-1.6", "1.6-2.1", "2.1-2.6"]
@@ -46,61 +47,104 @@ app.layout = dmc.NotificationsProvider(
             dcc.Tab(label="Visualization", value="visualization"),
             dcc.Tab(label="Recommender systems", value="recommender-model")
         ]),
-        html.Div(id="tab-content"),
+        html.Div([
+            html.Div([
+                html.Div([
+                    html.Div([
+                        html.Label("Genus"),
+                        dcc.Dropdown(id='dropdown-species', clearable=False),
+                    ], className='one-fourth-column'),
+                    html.Div([
+                        html.Label("Age spans"),
+                        dcc.Dropdown(options=AGE_SPANS, value=AGE_SPANS[0], multi=True, id="age_span"),
+                    ], className='one-fourth-column'),
+                    html.Div([
+                        html.Label("Threshold"),
+                        dcc.Slider(min=0, max=1, step=0.1, value=0, id='threshold'),
+                    ], className='one-fourth-column'),
+                    html.Div([
+                        html.Label("n highest recommendation scores"),
+                        dcc.Input(
+                            id="n-highest-rec-scores",
+                            value=0,
+                            type="number"
+                        )
+                    ], className='n-highest')
+                ], className='row'),
+                html.Div([
+                    html.Div([dcc.Graph(id='graph-content'), html.Div([], id='site-summary')], className='half-column'),
+                    html.Div([html.Div(id='site-info')], className='half-column'),
+                ], className='row'),
+            ], id="div-visualization"),
+            html.Div([
+                html.Div([
+                    html.Label("Choose algorithm"),
+                    dcc.Dropdown(
+                        id="dropdown-algorithm",
+                        options=[
+                            "Matrix Factorization",
+                            "kNN",
+                            "Content-based Filtering"
+                        ],
+                        value="Matrix Factorization",
+                        clearable=False)
+                ]),
+                html.Div([
+                    html.Div([
+                        html.Div([
+                            html.Label("Epochs"),
+                            dcc.Input(
+                                id="input-mf-epochs",
+                                value=100,
+                                type="number",
+                            ),
+                        ]),
+                        html.Div([
+                            html.Label("Output probabilities"),
+                            dcc.RadioItems([
+                                "Yes", "No"
+                            ], "Yes", id="radio-mf-output-prob")
+                        ]),
+                        html.Div([
+                            html.Button("Run", id="button-mf-run", n_clicks=0)
+                        ])
+                    ], id="div-mf"),
+                    html.Div([
+                        html.Div([
+                            html.Label("Top k"),
+                            dcc.Input(
+                                id="input-knn-k",
+                                value=15,
+                                type="number",
+                            )
+                        ]),
+                        html.Div([
+                            html.Label("Output probabilities"),
+                            dcc.RadioItems([
+                                "Yes", "No"
+                            ], "Yes", id="radio-knn-output-prob")
+                        ]),
+                        html.Div([
+                            html.Button("Run", id="button-knn-run", n_clicks=0)
+                        ])
+                    ], id="div-knn")
+                ], id="recommender-params")
+            ], id="div-recommender")
+        ], id="tab-content"),
     ]), position='top-right'
 )
 
 @callback(
-    Output("tab-content", "children"),
+    Output("div-visualization", "style"),
+    Output("div-recommender", "style"),
     Input("tabs", "value"),
 )
 def render_content(tab):
     if tab == "visualization":
-        return html.Div([
-            html.Div([
-                html.Div([
-                    html.Label("Genus"),
-                    dcc.Dropdown(id='dropdown-species', clearable=False),
-                ], className='one-fourth-column'),
-                html.Div([
-                    html.Label("Age spans"),
-                    dcc.Dropdown(options=AGE_SPANS, value=AGE_SPANS[0], multi=True, id="age_span"),
-                ], className='one-fourth-column'),
-                html.Div([
-                    html.Label("Threshold"),
-                    dcc.Slider(min=0, max=1, step=0.1, value=0, id='threshold'),
-                ], className='one-fourth-column'),
-                html.Div([
-                    html.Label("n highest recommendation scores"),
-                    dcc.Input(
-                        id="n-highest-rec-scores",
-                        value=0,
-                        type="number"
-                    )
-                ], className='n-highest')
-            ], className='row'),
-            html.Div([
-                html.Div([dcc.Graph(id='graph-content'), html.Div([], id='site-summary')], className='half-column'),
-                html.Div([html.Div(id='site-info')], className='half-column'),
-            ], className='row'),
-        ])
+        return dict(display=True), dict(display="none")
     
     if tab == "recommender-model":
-        return html.Div([
-            html.Div([
-                html.Label("Choose algorithm"),
-                dcc.Dropdown(
-                    id="dropdown-algorithm",
-                    options=[
-                        "Matrix Factorization",
-                        "kNN",
-                        "Content-based Filtering"
-                    ],
-                    value="Matrix Factorization",
-                    clearable=False)
-            ]),
-            html.Div(id="recommender-params")
-        ])
+        return dict(display="none"), dict(display=True)
 
 @callback(
     Output("dropdown-species", "options"),
@@ -111,24 +155,25 @@ def update_options(df):
     if df is None:
         raise PreventUpdate
     df = pd.DataFrame(df)
-    return list(df.columns), df.columns[7]
+    return list(df.columns), df.columns[0]
 
 @callback(
     Output('graph-content', 'figure'),
     Output('notifications-container', 'children'),
     Input('dropdown-species', 'value'),
     Input('threshold', 'value'),
-    Input("genera-occurrence-data", "data"),
+    State("prediction-data", "data"),
     Input("sites-meta-data", "data"),
     Input("age_span", "value"),
     Input("n-highest-rec-scores", "value"),
+    Input("div-visualization", "style"),
 )
-def update_graph(genera, threshold, df, sites_df, age_spans, n):
-    if df is None:
+def update_graph(genera, threshold, df, sites_df, age_spans, n, viz_style):
+    if df is None or viz_style == dict(style="none"):
         raise PreventUpdate
     
     dff = pd.concat([pd.DataFrame(df), pd.DataFrame(sites_df)], axis=1)
-    
+
     # Preprocess data
     gdff = preprocess_data(dff, genera, threshold)
 
@@ -206,30 +251,44 @@ def update_site_info(clickData):
     ], recommendations_html
 
 @callback(
-    Output("recommender-params", "children"),
+    Output("div-mf", "style"),
+    Output("div-knn", "style"),
     Input("dropdown-algorithm", "value")
 )
 def render_params(algorithm):
     if algorithm == "Matrix Factorization":
-        return html.Div([
-            html.Div([
-                html.Label("Epochs"),
-                dcc.Input(
-                    id="input-mf-epochs",
-                    value=100,
-                    type="number",
-                ),
-            ]),
-            html.Div([
-                html.Label("Output probabilities"),
-                dcc.RadioItems([
-                    "Yes", "No"
-                ], "Yes", id="radio-mf-output-prob")
-            ]),
-            html.Div([
-                html.Button("Run", id="button-mf-run", n_clicks=0)
-            ])
-        ])
+        return dict(display=True), dict(display="none")
+    
+    if algorithm == "kNN":
+        return dict(display="none"), dict(display=True)
+    
+@callback(
+    Output("prediction-data", "data"),
+    Input("button-mf-run", "n_clicks"),
+    Input("button-knn-run", "n_clicks"),
+    State("input-mf-epochs", "value"),
+    State("radio-mf-output-prob", "value"),
+    State("radio-knn-output-prob", "value"),
+    State("input-knn-k", "value"),
+    State("dropdown-algorithm", "value"),
+    State("genera-occurrence-data", "data"),
+)
+def run_recommender(n_clicks_mf, n_clicks_knn, epochs, output_prob_mf, output_prob_knn, k, model, df):
+    if df is None:
+        raise PreventUpdate
+    dff = pd.DataFrame(df)
+
+    if model == "Matrix Factorization":
+        if n_clicks_mf == 0:
+            raise PreventUpdate
+        df_output = get_recommend_list_mf(dff, output_prob_mf, epochs)
+
+    elif model == "kNN":
+        if n_clicks_knn == 0:
+            raise PreventUpdate
+        df_output = get_recommend_list_knn(dff, output_prob_knn, k)
+
+    return df_output.to_dict("records")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8010)
