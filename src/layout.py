bba15a07b64cf6bd3ffa5d9545b6f34adc552dbc
Akihiro Tanaka <akihiro.fin@gmail.com>
2024-03-31 09:17:58 +0300

    Refactorization

---
+++ src/layout.py
@@ -0,0 +1,122 @@
+import dash_mantine_components as dmc
+from dash import html, dcc
+
+AGE_SPANS = ["0-0.1", "0.1-0.6", "0.6-1.1", "1.1-1.6", "1.6-2.1", "2.1-2.6"]
+
+def get_layout():
+    return dmc.NotificationsProvider(
+    html.Div([
+        html.Div(id='notifications-container'),
+        html.H1(children='FossilNOW', style={'textAlign': 'center'}),
+        dcc.Store(id="genera-occurrence-data"),
+        dcc.Store(id="genera-info-data"),
+        dcc.Store(id="sites-meta-data"),
+        dcc.Store(id="prediction-data"),
+        dcc.Tabs(id="tabs", value="data", children=[
+            dcc.Tab(label="Data", value="data"),
+            dcc.Tab(label="Visualization", value="visualization"),
+            dcc.Tab(label="Recommender systems", value="recommender-model")
+        ]),
+        html.Div([
+            html.Div([
+                dcc.Upload(
+                    id="upload-data",
+                    children=html.Div([
+                        "Drag and Drop or ",
+                        html.A("Select Files")
+                    ]),
+                ),
+                dcc.Dropdown(id="df-dropdown", options=["Genera occurrences at sites", "Genera information"], value="Genera occurrences at sites", clearable=False),
+                html.Label("N meta data columns from the end of the dataframe:"),
+                dcc.Input(
+                    id="n-metacolumns",
+                    type="number",
+                    placeholder="n-meta data columns",
+                    value=0,
+                ),
+            ], id="div-data"),
+            html.Div([
+                html.Div([
+                    html.Div([
+                        html.Label("Genus"),
+                        dcc.Dropdown(id='dropdown-species', clearable=False),
+                    ], className='one-fourth-column'),
+                    html.Div([
+                        html.Label("Age spans"),
+                        dcc.Dropdown(options=AGE_SPANS, value=AGE_SPANS[0], multi=True, id="age_span"),
+                    ], className='one-fourth-column'),
+                    html.Div([
+                        html.Label("Threshold"),
+                        dcc.Slider(min=0, max=1, step=0.1, value=0, id='threshold'),
+                    ], className='one-fourth-column'),
+                    html.Div([
+                        html.Label("n highest recommendation scores"),
+                        dcc.Input(
+                            id="n-highest-rec-scores",
+                            value=0,
+                            type="number"
+                        )
+                    ], className='n-highest')
+                ], className='row'),
+                html.Div([
+                    html.Div([dcc.Graph(id='graph-content'), html.Div([], id='site-summary')], className='half-column'),
+                    html.Div([html.Div(id='site-info')], className='half-column'),
+                ], className='row'),
+            ], id="div-visualization"),
+            html.Div([
+                html.Div([
+                    html.Label("Choose algorithm"),
+                    dcc.Dropdown(
+                        id="dropdown-algorithm",
+                        options=[
+                            "Matrix Factorization",
+                            "kNN",
+                            "Content-based Filtering"
+                        ],
+                        value="Matrix Factorization",
+                        clearable=False)
+                ]),
+                html.Div([
+                    html.Div([
+                        html.Div([
+                            html.Label("Epochs"),
+                            dcc.Input(
+                                id="input-mf-epochs",
+                                value=100,
+                                type="number",
+                            ),
+                        ]),
+                        html.Div([
+                            html.Label("Output probabilities"),
+                            dcc.RadioItems([
+                                "Yes", "No"
+                            ], "Yes", id="radio-mf-output-prob")
+                        ]),
+                        html.Div([
+                            html.Button("Run", id="button-mf-run", n_clicks=0)
+                        ])
+                    ], id="div-mf"),
+                    html.Div([
+                        html.Div([
+                            html.Label("Top k"),
+                            dcc.Input(
+                                id="input-knn-k",
+                                value=15,
+                                type="number",
+                            )
+                        ]),
+                        html.Div([
+                            html.Label("Output probabilities"),
+                            dcc.RadioItems([
+                                "Yes", "No"
+                            ], "Yes", id="radio-knn-output-prob")
+                        ]),
+                        html.Div([
+                            html.Button("Run", id="button-knn-run", n_clicks=0)
+                        ])
+                    ], id="div-knn")
+                ], id="recommender-params")
+            ], id="div-recommender")
+        ], id="tab-content"),
+    ]), position='top-right'
+)
