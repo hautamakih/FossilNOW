ccc108d423eb72bf2804cbaa71311072d2edea5c
Henri Hautamäki <henri.juho.hautamaki@gmail.com>
2024-04-08 11:54:54 +0300

    Created a file for Hybrid Algorithm

---
+++ code/models/CF/Hybrid/CBF_CF_hybrid.py
@@ -0,0 +1,28 @@
+from models.CBF.ContentBasedFiltering import ContentBasedFiltering
+from surprise import KNNBaseline
+
+import numpy as np
+import pandas as pd
+
+class CbfCfHybrid:
+    def __init__(self):
+        pass
+
+    def fit(self, site_data: pd.DataFrame, genus_data: pd.DataFrame, n_site_columns: int, normalization: str="min-max"):
+        # Fit the Content Based Filtering
+        cbf = ContentBasedFiltering()
+        cbf.fit(
+            site_data = site_data,
+            genus_data = genus_data, 
+            n_site_columns = n_site_columns, 
+            normalization = "min-max"
+        )
+
+        self.cbf_scores = cbf.get_recommendations(matrix_form=True)
+
+        # Fit the kNN Collaborative Filtering
+
+        # Combine the results
+
+    def predict(self):
+        pass
